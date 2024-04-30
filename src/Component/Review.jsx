07381068e310271f8dc85f56c802831b5294c457
Golam Muntakim <seams123456789@gmail.com>
2024-04-30 02:39:07 -0700

    customer review updated

--- src/Component/Review.jsx
+++ src/Component/Review.jsx
@@ -1,126 +1,111 @@
+import Marquee from "react-fast-marquee";
 
 
 const Review = () => {
     return (
         <>
         <div className="flex flex-col lg:flex-row lg:justify-around items-center">
-        <div className="mt-8 mx-auto ">
+
+       <Marquee>
+	   <div className="mt-8 mx-auto flex gap-4">
             
-            <div className="flex flex-col max-w-xl p-8 shadow-sm rounded-xl lg:p-12 bg-green-700 text-gray-100 
-			w-[350px] lg:w-[400px] h-[400px]">
-	<div className="flex flex-col w-full">
-		<h2 className="text-3xl font-semibold text-center">Customer reviews</h2>
-		<div className="flex flex-wrap items-center mt-2 mb-1 space-x-2">
-			<div className="flex">
-				<button type="button" title="Rate 1 stars" aria-label="Rate 1 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 3 stars" aria-label="Rate 3 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 4 stars" aria-label="Rate 4 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-gray-600">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 5 stars" aria-label="Rate 5 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-6 h-6 text-gray-600">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
+			<div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+		<div className="flex justify-between p-4">
+			<div className="flex space-x-4">
+				<div>
+					<img src="https://i.ibb.co/JcZfDdn/warner.webp" alt="" className="object-cover w-12 h-12 rounded-full bg-gray-500" />
+				</div>
+				<div>
+					<h4 className="font-bold">Devid Warner</h4>
+					<span className="text-xs text-gray-400">2 days ago</span>
+				</div>
 			</div>
-			<span className="text-gray-400">3 out of 5</span>
+			<div className="flex items-center space-x-2 text-yellow-500">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+					<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+				</svg>
+				<span className="text-xl font-bold">4.5</span>
+			</div>
 		</div>
-		<p className="text-sm text-gray-400">861 global ratings</p>
-		<div className="flex flex-col mt-4">
-			<div className="flex items-center space-x-1">
-				<span className="flex-shrink-0 w-12 text-sm">5 star</span>
-				<div className="flex-1 h-4 overflow-hidden rounded-sm bg-gray-700">
-					<div className="bg-orange-700 h-4 w-5/6"></div>
-				</div>
-				<span className="flex-shrink-0 w-12 text-sm text-right">83%</span>
-			</div>
-			<div className="flex items-center space-x-1">
-				<span className="flex-shrink-0 w-12 text-sm">4 star</span>
-				<div className="flex-1 h-4 overflow-hidden rounded-sm bg-gray-700">
-					<div className="bg-orange-700 h-4 w-4/6"></div>
-				</div>
-				<span className="flex-shrink-0 w-12 text-sm text-right">67%</span>
-			</div>
-			<div className="flex items-center space-x-1">
-				<span className="flex-shrink-0 w-12 text-sm">3 star</span>
-				<div className="flex-1 h-4 overflow-hidden rounded-sm bg-gray-700">
-					<div className="bg-orange-700 h-4 w-3/6"></div>
-				</div>
-				<span className="flex-shrink-0 w-12 text-sm text-right">50%</span>
-			</div>
-			<div className="flex items-center space-x-1">
-				<span className="flex-shrink-0 w-12 text-sm">2 star</span>
-				<div className="flex-1 h-4 overflow-hidden rounded-sm bg-gray-700">
-					<div className="bg-orange-700 h-4 w-2/6"></div>
-				</div>
-				<span className="flex-shrink-0 w-12 text-sm text-right">33%</span>
-			</div>
-			<div className="flex items-center space-x-1">
-				<span className="flex-shrink-0 w-12 text-sm">1 star</span>
-				<div className="flex-1 h-4 overflow-hidden rounded-sm bg-gray-700">
-					<div className="bg-orange-700 h-4 w-1/6"></div>
-				</div>
-				<span className="flex-shrink-0 w-12 text-sm text-right">17%</span>
-			</div>
+		<div className="p-4 space-y-2 text-sm text-gray-400">
+			<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+			<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
 		</div>
 	</div>
-        </div>
-        </div>
-        {/* <div className="flex flex-col p-4 shadow-sm rounded-xl lg:p-4 bg-green-700 text-gray-100 mx-auto mt-4 w-[350px] lg:w-[400px] h-[400px]">
-	<div className="flex flex-col items-center w-full">
-		<h2 className="text-3xl font-semibold text-center">Your opinion matters!</h2>
-		<div className="flex flex-col items-center py-6 space-y-3">
-			<span className="text-center">How was your experience?</span>
-			<div className="flex space-x-3">
-				<button type="button" title="Rate 1 stars" aria-label="Rate 1 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 2 stars" aria-label="Rate 2 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 3 stars" aria-label="Rate 3 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 4 stars" aria-label="Rate 4 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-yellow-500">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
-				<button type="button" title="Rate 5 stars" aria-label="Rate 5 stars">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-10 h-10 text-gray-600">
-						<path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-					</svg>
-				</button>
+			<div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+		<div className="flex justify-between p-4">
+			<div className="flex space-x-4">
+				<div>
+					<img src="https://i.ibb.co/Nt5FwTh/virat.webp" alt="" className="object-cover w-12 h-12 rounded-full bg-gray-500" />
+				</div>
+				<div>
+					<h4 className="font-bold">Virat Kohli</h4>
+					<span className="text-xs text-gray-400">7 days ago</span>
+				</div>
+			</div>
+			<div className="flex items-center space-x-2 text-yellow-500">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+					<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+				</svg>
+				<span className="text-xl font-bold">4.2</span>
 			</div>
 		</div>
-		<div className="flex flex-col w-full">
-			<textarea rows="3" placeholder="Message..." className="p-4 rounded-md resize-none text-gray-100 bg-gray-900"></textarea>
-			<button type="button" className="py-4 my-8 font-semibold rounded-md text-white-900 bg-green-950">Leave feedback</button>
+		<div className="p-4 space-y-2 text-sm text-gray-400">
+			<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+			<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
 		</div>
 	</div>
-	
-        </div> */}
+			<div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+		<div className="flex justify-between p-4">
+			<div className="flex space-x-4">
+				<div>
+					<img src="https://i.ibb.co/wgknXht/fizz.webp" alt="" className="object-cover w-12 h-12 rounded-full bg-gray-500" />
+				</div>
+				<div>
+					<h4 className="font-bold">Fizz</h4>
+					<span className="text-xs text-gray-400">15 days ago</span>
+				</div>
+			</div>
+			<div className="flex items-center space-x-2 text-yellow-500">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+					<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+				</svg>
+				<span className="text-xl font-bold">4.0</span>
+			</div>
+		</div>
+		<div className="p-4 space-y-2 text-sm text-gray-400">
+			<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+			<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+		</div>
+	</div>
+			<div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md divide-gray-700 bg-gray-900 text-gray-100">
+		<div className="flex justify-between p-4">
+			<div className="flex space-x-4">
+				<div>
+					<img src="https://i.ibb.co/84T21SS/dhoni.webp" alt="" className="object-cover w-12 h-12 rounded-full bg-gray-500" />
+				</div>
+				<div>
+					<h4 className="font-bold">MS Dhoni</h4>
+					<span className="text-xs text-gray-400">2 days ago</span>
+				</div>
+			</div>
+			<div className="flex items-center space-x-2 text-yellow-500">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+					<path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+				</svg>
+				<span className="text-xl font-bold">4.5</span>
+			</div>
+		</div>
+		<div className="p-4 space-y-2 text-sm text-gray-400">
+			<p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+			<p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+		</div>
+	</div>
+			   
+			</div>
+
+	   </Marquee>
+
         </div>
 
         <section className="p-6 text-gray-100">
